10bc0b6c7a16d03ad87c45bda3542048ccb8f360
claytoncalixto <claytonrcalixto@gmail.com>
2021-07-27 23:58:05 -0300

    04-06 Course

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -13,3 +13,4 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES('Bootcamp HTML', 'https://url.gratis/BqQup0', 'https://url.gratis/ZFKNwd' );
