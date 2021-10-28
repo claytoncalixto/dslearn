7ebbe3d00635134af78bdf6928cdd9e210c1af15
claytoncalixto <claytonrcalixto@gmail.com>
2021-10-28 11:18:38 -0300

    Customized authorization in the service level - Part 2

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
@@ -151,4 +151,13 @@ public class User implements UserDetails, Serializable {
 	public boolean isEnabled() {
 		return true;
 	}
+	
+	public boolean hasHole(String rolename) {
+		for(Role role: roles) {
+			if(role.getAuthority().equals(rolename)) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
